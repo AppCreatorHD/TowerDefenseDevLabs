0f9ac1c397b271d651f755927de5c1a40cae6f04
Skyzo <asus@pcasus>
2015-06-09 16:21:58 +0200

    attributs rajoutés dans ASSAILLANTS et ses classes filles (Assaillants1 etc...), reste à ajouter les méthodes dans la classe mère : ASSAILLANTS

--- TowerDefense/src/TowerDefense/Assaillant1.java
+++ TowerDefense/src/TowerDefense/Assaillant1.java
@@ -2,4 +2,11 @@ package TowerDefense;
 
 class Assaillant1 extends ASSAILLANTS {
 
+	protected int nombre_hp;
+	protected int nombre_degats;
+	protected float vitesse_deplacement;
+	protected int portee;
+	protected float resistance;
+	protected int ressources_gagnees;
+	
 }
--- TowerDefense/src/TowerDefense/Assaillant4.java
+++ TowerDefense/src/TowerDefense/Assaillant4.java
@@ -1,5 +1,12 @@
 package TowerDefense;
 
 class Assaillant4 extends ASSAILLANTS {
+	
+	protected int nombre_hp;
+	protected int nombre_degats;
+	protected float vitesse_deplacement;
+	protected int portee;
+	protected float resistance;
+	protected int ressources_gagnees;
 
 }

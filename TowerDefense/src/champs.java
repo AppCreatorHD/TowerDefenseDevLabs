5d77e5b7f91b42cdd29664927a8f456d0ad47a5a
Guillaume <Guillaume@10.162.128.60>
2015-06-09 14:57:53 +0200

    test

---
+++ TowerDefense/src/champs.java
@@ -0,0 +1,7 @@
+public class champs extends sol {
+	public static char DESSIN_SOL = 'C';
+	
+	public champs(){
+		this.dessin = DESSIN_SOL;
+	}
+}
---
+++ TowerDefense/src/chemin.java
@@ -0,0 +1,3 @@
+public class chemin extends sol {
+
+}

3f158b9a57ff318e4c32ebf469e0062b243e1e93
Guillaume <Guillaume@10.162.128.49>
2015-06-10 17:24:53 +0200

    tentative de chrono

--- TowerDefense/src/TowerDefense/Carte.java
+++ TowerDefense/src/TowerDefense/Carte.java
@@ -2,11 +2,16 @@ package TowerDefense;
 
 import java.awt.Color;
 import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Carte extends JFrame {
 	public Carte() {
@@ -14,5 +19,69 @@ public class Carte extends JFrame {
 		setResizable(false);
 		setTitle("Tower Defense");
 		getContentPane().setBackground(Color.BLACK);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBackground(Color.BLACK);
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon("Image/Map1-1.png"));
+		
+		JButton btnNewButton = new JButton("Tour1");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JButton btnTour = new JButton("Tour2");
+		btnTour.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnTour_1 = new JButton("Tour3");
+		
+		JButton btnTour_2 = new JButton("Tour4");
+		
+		JButton btnAmlior = new JButton("Amel.");
+		
+		JButton btnSuppr = new JButton("Suppr");
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addGap(152)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnTour, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnTour_1, GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
+					.addGap(18)
+					.addComponent(btnTour_2, GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
+					.addGap(18)
+					.addComponent(btnAmlior, GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
+					.addGap(18)
+					.addComponent(btnSuppr, GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
+					.addGap(154))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(210)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 375, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(209, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(71, Short.MAX_VALUE)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 375, GroupLayout.PREFERRED_SIZE)
+					.addGap(44)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnTour, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnTour_1, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnTour_2, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnAmlior, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnSuppr, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE))
+					.addGap(26))
+		);
+		getContentPane().setLayout(groupLayout);
 	}
 }
--- TowerDefense/src/TowerDefense/Carte_2.java
+++ TowerDefense/src/TowerDefense/Carte_2.java
@@ -1,15 +1,50 @@
 package TowerDefense;
 
-import java.awt.Color;
-import java.awt.Toolkit;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+ 
 import javax.swing.JFrame;
-
-public class Carte_2 extends JFrame {
-	public Carte_2() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage("Image/imgDL.jpg"));
-		setResizable(false);
-		setTitle("Tower Defense");
-		getContentPane().setBackground(Color.BLACK);
+import javax.swing.JLabel;
+import javax.swing.Timer;
+ 
+public class Carte_2 extends JFrame{
+	//Declaration de variables
+	private int heure=0,minute=0,seconde=0;
+	private int delais=1000;
+ 
+	private JLabel label=new JLabel(""+heure+":"+minute+":"+seconde);
+	private ActionListener tache_timer;
+	private Timer timer1;
+ 
+ 
+	public Carte_2(){
+		//Ajout de notre Jlabel au composant fenetre
+		this.getContentPane().add(label);
+		chrono();
 	}
+ 
+	//Methode pour lancer notre chrono
+	public void chrono(){
+		tache_timer= new ActionListener()  {
+		  public void actionPerformed(ActionEvent e1)  {
+		    seconde++;
+		     if(seconde==60)  {
+		          seconde=0;
+		          minute++;
+		      }
+		  if(minute==60) {
+		      minute=0;
+		      heure++;
+		   }
+		  //Afficher le chrono dans un JLabel
+		  label.setText(heure+":"+minute+":"+seconde);
+ 
+		 }
+     };
+     //Action et temps execution de la tache
+     timer1=new Timer(delais,tache_timer);
+     //Demarrer le chrono
+     timer1.start();
+	}
+ 
 }

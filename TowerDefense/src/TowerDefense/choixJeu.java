4dc6eeb06b5097bf5bff4fa54974c2f833a727c5
Guillaume <Guillaume@10.162.128.49>
2015-06-10 12:15:11 +0200

    rechoix carte

--- TowerDefense/src/TowerDefense/choixJeu.java
+++ TowerDefense/src/TowerDefense/choixJeu.java
@@ -1,11 +1,19 @@
 package TowerDefense;
 
 import javax.swing.JFrame;
+
 import java.awt.Color;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
-import javax.swing.LayoutStyle.ComponentPlacement;
+/*import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Panel;
+import java.awt.Canvas;
+import java.awt.Label;*/
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+//import javax.swing.JPanel;
 
 public class choixJeu extends JFrame {
 	public choixJeu() {
@@ -17,29 +25,44 @@ public class choixJeu extends JFrame {
 		JButton btnCarte_1 = new JButton("Carte 2");
 		
 		JButton btnCarte_2 = new JButton("Carte 3");
+		
+		JButton btnRetour = new JButton("Retour");
+		btnRetour.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				Fenetre fenetre = new Fenetre();
+				fenetre.setSize(800,600);
+				fenetre.setVisible(true);
+				fenetre.setLocationRelativeTo(null);
+			}
+		});
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(73)
-					.addComponent(btnCarte, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(btnCarte, GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
 					.addGap(98)
 					.addComponent(btnCarte_1, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
 					.addGap(97)
 					.addComponent(btnCarte_2, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
 					.addGap(66))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(677, Short.MAX_VALUE)
+					.addComponent(btnRetour, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap(312, Short.MAX_VALUE)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnCarte_1, GroupLayout.PREFERRED_SIZE, 68, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnCarte_2, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnCarte, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE))
-					.addGap(180))
+					.addGap(134)
+					.addComponent(btnRetour, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
 		);
 		getContentPane().setLayout(groupLayout);
 	}
-
 }

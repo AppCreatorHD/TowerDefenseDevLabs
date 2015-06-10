f2de033adb39550d82e7103c6594b45f2b637c21
Skyzo <asus@pcasus>
2015-06-10 11:15:53 +0200

    Valeurs aux attributs

--- TowerDefense/src/TowerDefense/Assaillant5.java
+++ TowerDefense/src/TowerDefense/Assaillant5.java
@@ -2,12 +2,12 @@ package TowerDefense;
 
 class Assaillant5 extends ASSAILLANTS {
 	
-	protected int nombre_hp;
-	protected int nombre_degats;
-	protected float vitesse_deplacement;
-	protected int portee;
-	protected float resistance;
-	protected int ressources_gagnees;
+	protected int nombre_hp = 50;
+	protected int nombre_degats = 100;
+	protected float vitesse_deplacement = 10;
+	protected int portee = 6;
+	protected float resistance = 0;
+	protected int ressources_gagnees = 50;
 
 
 }

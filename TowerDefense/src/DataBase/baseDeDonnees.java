e66e697f868f9e6f61f1919392bfe3b9af46e65b
Guillaume <Guillaume@10.162.128.60>
2015-06-10 12:14:02 +0200

    ajout connexion BDD

---
+++ TowerDefense/src/DataBase/baseDeDonnees.java
@@ -0,0 +1,7 @@
+package DataBase;
+
+public class baseDeDonnees {
+	private static String url = "jdbc:mysql://10.162.128.60/towerdevlabs";
+	private static String root = "root";
+	private static String password = "admin";
+}

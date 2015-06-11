b5646d5ad18e6db67e836435d45d6b472547b5ac
Guillaume <Guillaume@10.162.128.49>
2015-06-11 16:48:46 +0200

    correction

--- TowerDefense/src/TowerDefense/Assaillant1.java
+++ TowerDefense/src/TowerDefense/Assaillant1.java
@@ -1,19 +1,12 @@
 package TowerDefense;
 
-class Assaillant1 extends ASSAILLANTS {
-
-	public class Assaillant1 extends ASSAILLANTS{
+public class Assaillant1 extends ASSAILLANTS{
 	
-	this.nombre_hp = 5;
-	this.nombre_degats = 25;
-	this.vitesse_deplacement = 1;
-	this.portee = 2;
-	this.resistance = 0;
-	this.ressources_gagnees = 10;
-	
-	}
+	int nombre_hp = 5;
+	int nombre_degats = 25;
+	int vitesse_deplacement = 1;
+	int portee = 2;
+	int resistance = 0;
+	int ressources_gagnees = 10;
 	
 }
-
-}
-
